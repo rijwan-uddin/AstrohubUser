95d541ccc90ee85dc97e1d5431b9e7962286ca44
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-05-23 23:54:36 +0600

    addUser method created

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,9 +1,18 @@
+import 'package:astrohub_user/db/db_helper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/app_user.dart';
 
 class UserProvider extends ChangeNotifier{
 
 
-  Future
+  Future<void> addUser({required User user, String? name, String? phone} ) {
+final appUser = AppUser(uid: user.uid, email:user.email!,userName: name,
+phone: phone,
+  userCreationTime: Timestamp.fromDate(user.metadata.creationTime!),
+);
+return DbHelper.addUser(appUser);
+}
 
 }
-//189 5:40
