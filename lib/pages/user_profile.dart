479c29a62f097a2f04616f63e74390d2a9dd17a2
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-09-04 20:04:40 +0600

    widget function added

---
+++ lib/pages/user_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:provider/provider.dart';
+import 'package:astrohub_user/providers/user_provider.dart';
+import 'package:astrohub_user/utils/widget_functions.dart';
+
+class UserProfilePage extends StatelessWidget {
+  static const String routeName = 'profile';
+  const UserProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
